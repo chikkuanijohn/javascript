39d894c267a1a5d86e1253e6ef39791fbd544cbf
chikkuanijohn <chikkuanijohn1@gamil.com>
2024-09-18 18:35:03 +0530

    new file

---
+++ script.js
@@ -0,0 +1,110 @@
+// var a
+// a=10
+// let a=10
+// a=10
+// Const a=10
+// alert(a)
+    
+
+// let a=10.5
+// alert(typeof a)
+
+// let a='welcome'
+// alert(typeof a)
+
+// let a='1234'
+// alert(typeof a)
+
+// let a=true
+// alert(typeof a)
+
+// let a=false
+// alert(typeof a)
+
+// let a
+// alert(typeof a)
+
+// let a
+// alert(a)
+
+
+// let a=[1,2,3,4]
+// alert(typeof a)
+
+// let b={name:'abc'}
+// alert(typeof b)
+
+// let c=12345678900
+// alert(typeof c)
+
+
+
+
+
+// let a='welcome'
+// let b=10
+// alert(a+b)
+
+
+// let a=20
+// let b=10
+// alert(a-b)
+
+// let a=10
+// let b=2
+// alert(a/b)
+
+
+// let a=10
+// let b=2
+// alert(a*b)
+
+
+// let a=10
+// let b=2
+// alert(a%b)
+
+
+// let a=10
+// let b=2
+// a++
+// alert(a)
+
+
+// let a=10
+// let b=2
+// a--
+// alert(a)
+
+
+// let a=10
+// let b=2
+// a+=10
+// a-=10
+// a*=10
+// a/=10
+// a%=10
+
+// alert(a)
+
+
+
+// let a=10
+// let b=10
+// alert(a==b)
+
+// let a=10
+// let b='10'
+// alert(a===b)
+
+
+// let a=10
+// let b='10'
+// alert(a!==10)
+
+
+let a=10
+let b='10'
+// alert(a==&& a>8)
+// alert(a==b||a>8)
+alert(!a==b)
