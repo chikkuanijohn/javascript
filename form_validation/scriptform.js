ce66bd36f033919068bfd32ded168530578b66cf
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-09-23 17:53:47 +0530

    new file

--- form_validation/scriptform.js
+++ form_validation/scriptform.js
@@ -1,4 +1,30 @@
 function valid(){
-    document.getElementById("phno_error").innerHTML=""
-    name=
+    document.getElementById("num_error").innerHTML=""
+    document.getElementById("pin_error").innerHTML=""
+    document.getElementById("email_error").innerHTML=""
+
+    name=document.getElementById("name").value
+    phno=document.getElementById("phno").value
+    email=document.getElementById("email").value
+    pin=document.getElementById("pin").value
+    console.log(name,phno,email,pin);
+    if(phno.length!=10){
+        document.getElementById("num_error").innerHTML="*invalid number"
+    }
+    else{
+        if(!phno.match('[6-9].{9}')){
+            document.getElementById("num_error").innerHTML="*invalid number"
+        }
+    }
+    
+    if(pin.length!=6){
+        document.getElementById("pin_error").innerHTML="*invalid pin"
+    }
+    
+    if(email.match("@gmail.com")){
+    }
+    else{
+        document.getElementById("email_error").innerHTML="invalid email"
+    }
+
 }
---
+++ list/scriptmethods.js
@@ -0,0 +1,31 @@
+l=[1,2,3,4,5,6]
+
+// console.log(typeof l);
+// console.log(l[0]);
+// console.log(l[3]);
+// console.log(l.length)
+// a=l.toString()
+// console.log(typeof a);
+// l.pop()
+// console.log(l);
+// l.push(10)
+
+// console.log(l);
+// l.shift()
+// console.log(1[0]);
+// l.unshift(101)
+// console.log(l[0]);
+// delete l[0]
+// console.log(l);
+
+// for (let i in l){
+//      console.log(l[i]);
+    
+// }
+
+// for (let i of l){
+//     console.log(i);
+    
+// }
+
+
